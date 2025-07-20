0a4e7b0b67827a09875f7e53e29e34f890f76b57
abbasbayat0 <abbas0bayat@gmail.com>
2025-07-20 20:40:36 +0330

    single item done

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,11 +2,10 @@ import { v4 } from "uuid";
 import SingleItem from "./SingleItem";
 
 const Items = ({ items, removeItem }) => {
-  console.log(items);
   return (
     <div>
       {items.map((item) => {
-        return <SingleItem key={v4()} item={item} removeItems={removeItem} />;
+        return <SingleItem key={v4()} item={item} removeItem={removeItem} />;
       })}
     </div>
   );
--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const SingleItem = ({ item, removeItem }) => {
-  console.log(item);
-  return <div>{item.name}</div>;
+  const [isChecked, setIsChecked] = useState(item.completed);
+  return (
+    <div className="mt-10 flex justify-around">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p className={`${isChecked ? "line-through" : "font-bold "}`}>
+        {item.name}
+      </p>
+      <button onClick={removeItem(item.id)} className="text-red-400">
+        delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
